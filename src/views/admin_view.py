67c364a0304fecb29f1e164955e8cfe32045c394
jlucioDev <jluciodev@github.com>
2026-03-01 16:56:08 -0300

    Corrige sintaxe e atributos da View de Administracao

--- src/views/admin_view.py
+++ src/views/admin_view.py
@@ -4,26 +4,25 @@ import flet as ft
 class AdminView(ft.View):
 
     def __init__(self, page):
-        super().__init__(
-            route="/admin",
-            appbar=ft.AppBar(title=ft.Text("Página Administração"), bgcolor=ft.Colors.BLUE_400),
-            controls=[
-                ft.Row(
-                    expand=True,
-                    
-                    controls=[
-                        ft.Container(
-                            bgcolor=ft.Colors.BLUE_600, 
-                            width=300, 
-                            
-                        ),
-                        ft.Container(
-                            expand=True ,
-                            bgcolor=ft.Colors.GREEN_600, 
-                        )
-                ])
-            ],
-        )
+        super().__init__(route="/", bgcolor=ft.Colors.GREY_900)
+        self.appbar= ft.AppBar(title=ft.Text("Página HOME"), bgcolor=ft.Colors.BLUE_400),
+        self.controls=[
+            ft.Row(
+                expand=True,
+                
+                controls=[
+                    ft.Container(
+                        bgcolor=ft.Colors.BLUE_600, 
+                        width=300, 
+                        
+                    ),
+                    ft.Container(
+                        expand=True ,
+                        bgcolor=ft.Colors.GREEN_600, 
+                    )
+            ])
+        ]
+
     
     async def open_home_view(self, e):
         await self.page.push_route("/")
