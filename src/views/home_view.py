21aeddc70d990242c46a776216df9d056520118f
jlucioDev <jluciodev@github.com>
2026-03-01 16:41:13 -0300

    Corrige evento on_hover do Container e ajusta escala

--- src/views/home_view.py
+++ src/views/home_view.py
@@ -15,7 +15,9 @@ class HomeView(ft.View):
                     ft.Text("Selecione o seu perfil de acesso", size=20, color=ft.Colors.WHITE),
                     ft.Container(height=40), #Espaçador
                     ft.Row([
-                            self._create_card("Administração", ft.Icons.SETTINGS, "/admin")
+                            self._create_card("Administração", ft.Icons.SETTINGS, self.open_admin_view),
+                            self._create_card("Cliente", ft.Icons.RESTAURANT, self.open_admin_view),
+                            self._create_card("Cozinha", ft.Icons.KITCHEN, self.open_cozinha_view),
                             ],
                             alignment=ft.MainAxisAlignment.CENTER,
                             spacing=30
@@ -32,17 +34,34 @@ class HomeView(ft.View):
         
     async def open_cozinha_view(self, e):
         await self.page.push_route("/cozinha")
+    
 
-    def _create_card(self, title, icon, route):
+
+    def _create_card(self, title, icon, rota):
         card = ft.Container(
             content=ft.Column([
                 ft.Icon(icon=icon, size=60, color=ft.Colors.WHITE),
                 ft.Text(title, size=20, weight=ft.FontWeight.BOLD, color=ft.Colors.WHITE)
-            ], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                ], 
+                alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER),
+            
             width=250,
             height=200,
-
-            )
-
+            bgcolor=ft.Colors.ORANGE_800,
+            border_radius=15,
+            padding=20,
+            # ink=True absorve o evento de hover no backend do Flutter, 
+            # então precisamos desativá-lo para o on_hover da escala funcionar.
+            on_click=rota,
+            scale=1.0,
+            animate_scale=ft.Animation(duration=300, curve=ft.AnimationCurve.BOUNCE_OUT),
+            on_hover=self._on_hover
         )
         return card
+
+    def _on_hover(self, e):
+        # Transforma o e.data em texto para garantir a leitura ("true" ou True convertem para verificação segura)
+        is_hovering = str(e.data).lower() == "true"
+        # Mudei a escala pra 1.05 para o cartão não sair da tela (1.50 dobrava de tamanho)
+        e.control.scale = 1.05 if is_hovering else 1.0
+        e.control.update()
