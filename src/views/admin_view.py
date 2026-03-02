1b55dfe8ecd1af78d90d47f208365c44975b2da9
jlucioDev <jluciodev@github.com>
2026-03-01 21:16:32 -0300

    Implementa logica de selecao de itens do sidebar (on_click_itemSidebar)

--- src/views/admin_view.py
+++ src/views/admin_view.py
@@ -32,9 +32,9 @@ class AdminView(ft.View):
                                 ft.Container(height=50),
 
                                 ft.Container(content=ft.Column([
-                                    self._create_sidebarItens(ft.Icons.HOME_OUTLINED, "Dashboard", "/"),
-                                    self._create_sidebarItens(ft.Icons.INSERT_COMMENT_OUTLINED, "Cadastros", "/"),
-                                    self._create_sidebarItens(ft.Icons.KITCHEN_OUTLINED, "Cozinha", "/"),
+                                    self._create_sidebarItens(ft.Icons.HOME_OUTLINED, "Dashboard", True, "/"),
+                                    self._create_sidebarItens(ft.Icons.INSERT_COMMENT_OUTLINED, "Cadastros", False, "/"),
+                                    self._create_sidebarItens(ft.Icons.KITCHEN_OUTLINED, "Cozinha", False, "/"),
               
                                 ]), expand=True, height=100)
 
@@ -43,7 +43,10 @@ class AdminView(ft.View):
                         ),
                         ft.Container(
                             expand=True ,
-                            bgcolor=ft.Colors.BLACK, 
+                            bgcolor=ft.Colors.BLACK,
+                            content=ft.Column([
+                                self.abrir_dashboad()
+                            ])
                         )
                 ])
             ],
@@ -53,31 +56,33 @@ class AdminView(ft.View):
     async def open_home_view(self, e):
         await self.page.push_route("/")
 
-    def _create_sidebarItens(self, icon, text, rota):
+    def _create_sidebarItens(self, icon, text, is_checked, rota):
         card = ft.Container(padding=10, content=ft.Row([
             ft.Icon(icon=icon, size=25, color=ft.Colors.GREY_600),
             ft.Text(text, size=18, color=ft.Colors.GREY_600),
-        ]), 
-        bgcolor=ft.Colors.TRANSPARENT,
+        ]),
+        data = is_checked,
+        bgcolor=ft.Colors.TRANSPARENT if is_checked == False else ft.Colors.ORANGE_200,
         border_radius=10,
         height=50,
-        #scale=1.0,
         #animate_scale=ft.Animation(duration=100, curve=ft.AnimationCurve.BOUNCE_OUT),
-        on_hover=self._on_hover
+        on_hover=self._on_hover,
+        on_click=self.on_click_itemSidebar
         )
         return card
 
     def _on_hover(self, e: ft.ControlEvent):
         # Transforma o e.data em texto para garantir a leitura ("true" ou True convertem para verificação segura)
-        is_hovering = str(e.data).lower() == "true"
-        is_transparent = str(e.control.bgcolor) == "Colors.TRANSPARENT"
+        #is_hovering = str(e.data).lower() == "true"
+        if e.control.data == False:
+            is_transparent = str(e.control.bgcolor) == "Colors.TRANSPARENT"
 
-        # Mudei a escala pra 1.05 para o cartão não sair da tela (1.50 dobrava de tamanho)
-        #e.control.scale = 1.05 if is_hovering else 1.0
-        e.control.bgcolor=ft.Colors.ORANGE_800 if is_transparent else ft.Colors.TRANSPARENT 
-        e.control.content.controls[0].color = ft.Colors.GREY_300 if is_transparent else ft.Colors.GREY_600
-        e.control.content.controls[1].color = ft.Colors.GREY_300 if is_transparent else ft.Colors.GREY_600
-        e.control.update()
+            # Mudei a escala pra 1.05 para o cartão não sair da tela (1.50 dobrava de tamanho)
+            #e.control.scale = 1.05 if is_hovering else 1.0
+            e.control.bgcolor=ft.Colors.ORANGE_800 if is_transparent else ft.Colors.TRANSPARENT 
+            e.control.content.controls[0].color = ft.Colors.GREY_300 if is_transparent else ft.Colors.GREY_600
+            e.control.content.controls[1].color = ft.Colors.GREY_300 if is_transparent else ft.Colors.GREY_600
+            e.control.update()
 
     def _on_click_menu(self, e):
         isOpen = e.control.data == "open"
@@ -100,3 +105,17 @@ class AdminView(ft.View):
             ft.Text("Você está na DashBoard", size=50, color=ft.Colors.GREY_500)
         ])
         return card
+    def on_click_itemSidebar(self, e):
+        # limpar lista de itens do sidebar
+        sidebar_items = self.controls[0].controls[0].content.controls[2].content.controls
+        for item in sidebar_items:
+            item.data = False
+            item.bgcolor = ft.Colors.TRANSPARENT
+            item.content.controls[0].color = ft.Colors.GREY_600
+            item.content.controls[1].color = ft.Colors.GREY_600
+        
+        # mude a cor do item para ft.Colors.ORANGE_200
+        e.control.data = True
+        e.control.bgcolor = ft.Colors.ORANGE_200
+        
+        self.update()
